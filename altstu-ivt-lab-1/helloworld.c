c622868c0b1344109ebb70a760247fd869b6cbc7
User-W10-5898636 <User-W10-5898636@WIN-F2C1PCGA8V8>
2023-09-07 21:36:00 +0700

    Добавьте файлы проекта.

---
+++ altstu-ivt-lab-1/helloworld.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <locale.h>
+
+/**
+ * @brief Лр/ 1 Знакомство со средой разработки
+ * Исходный код на C
+ * @return Всегда 0
+*/
+int main() {
+	setlocale(LC_ALL, "ru");
+
+	printf("Hello world! Привет мир!\n");
+
+	int var = 42;
+	int unusing;
+}
